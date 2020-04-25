6fa437f2b3dcd81dd087deef83070a3859564f5e
Arshaq shakeel <codeavenue.frontend03@gmail.com>
2020-04-26 01:12:48 +0500

    Card Generalization & Show Data in Event List

--- AwesomeProject/EventCard.js
+++ AwesomeProject/EventCard.js
@@ -1,3 +1,46 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+// import propTypes from 'prop-ty?'
 import {formatDate,} from './api';
+
+
+const styles =StyleSheet.create({
+    card: {
+        marginTop: 50,
+      },
+      cardHeader: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+      },
+      date: {
+        color: 'red',
+      },
+
+})
+export default function EventCard(props){
+return(
+    <View 
+    style={styles.card}
+    
+    >
+
+        <View 
+        style={styles.cardHeader}
+        
+        >
+
+<Text 
+style={styles.date}
+
+>{formatDate(props.event.name)}</Text>
+<Text 
+// style={style.title}
+
+>{props.event.age}</Text>
+
+
+        </View>
+    </View>
+)
+}
--- AwesomeProject/EventList.js
+++ AwesomeProject/EventList.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import {Text, View, FlatList} from 'react-native';
+import EventCrad from './EventCard';
 
 let obj = [
   {
-    id: "1",
+    id: '1',
     name: 'Arshaq',
     age: 24,
   },
   {
-    id: "2",
+    id: '2',
     name: 'Arif',
     age: 22,
   },
@@ -18,19 +19,7 @@ class EventList extends Component {
     return (
       <FlatList
         data={obj}
-     
-        renderItem={({item, index}) =>  (
-          <View
-            style={{
-              flex: 1,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <Text keys={index}> {item.name}</Text>
-
-            <Text keys={index}>{item.age}</Text>
-          </View>
-        )}
+        renderItem={({item, index}) => <EventCrad event={item} />}
         keyExtractor={(item) => item.id}
       />
     );
